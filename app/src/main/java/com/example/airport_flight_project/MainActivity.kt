c221200e8dec615f5d8e91d1afe4e046f5a8b1dc
PilouSquaglia <pilou.squaglia@laposte.net>
2023-10-18 16:39:35 +0200

    first page

--- app/src/main/java/com/example/airport_flight_project/MainActivity.kt
+++ app/src/main/java/com/example/airport_flight_project/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.airport_flight_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Utils.generateAirportList()
+        setContentView(R.layout.first_page)
+        val airportList = Utils.generateAirportList()
+
+        val spinner = findViewById<Spinner>(R.id.airport_spinner)
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, airportList)
+        spinner.adapter = spinnerAdapter
     }
 }
