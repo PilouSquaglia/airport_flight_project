b9580b8cd688d02c7172d8ad50fc5e1dcab6439c
PilouSquaglia <pilou.squaglia@laposte.net>
2023-10-20 09:27:32 +0200

    add date select

--- app/src/main/java/com/example/airport_flight_project/MainActivity.kt
+++ app/src/main/java/com/example/airport_flight_project/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.airport_flight_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fromDateTextView: TextView
+    private lateinit var toDateTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_page)
@@ -14,5 +21,27 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.airport_spinner)
         val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, airportList)
         spinner.adapter = spinnerAdapter
+
+        fromDateTextView = findViewById(R.id.from_date)
+        toDateTextView = findViewById(R.id.to_date)
+
+        fromDateTextView.setOnClickListener { showDatePickerDialog(fromDateTextView) }
+        toDateTextView.setOnClickListener { showDatePickerDialog(toDateTextView) }
+    }
+
+    private fun showDatePickerDialog(textView: TextView) {
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            val selectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
+            textView.text = selectedDate
+        }
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            dateSetListener,
+            2024,
+            1,
+            6
+        ) // Définissez la date initiale ici
+        datePickerDialog.show()
     }
 }
