3b0eb9db1c96f78fb761dabf1cb94ae660bb051f
jeanForconi <forconijean@gmail.com>
2023-11-08 17:37:01 +0100

    mise en place d'une norification pour alerté l'utilisateur

--- app/src/main/java/com/example/airport_flight_project/MapViewModel.kt
+++ app/src/main/java/com/example/airport_flight_project/MapViewModel.kt
@@ -3,6 +3,7 @@ package com.example.airport_flight_project
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
